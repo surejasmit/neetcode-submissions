075c2064e53fefefdba1300c68130b66b1aeea4f
smit sureja <smitsureja472007@gmail.com>
2026-06-23 11:38:01 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            if(s[i] == '}') {
+                if(st.empty() || st.top() != '{')
+                    return false;
+                st.pop();
+            }
+
+            else if(s[i] == ')') {
+                if(st.empty() || st.top() != '(')
+                    return false;
+                st.pop();
+            }
+
+            else if(s[i] == ']') {
+                if(st.empty() || st.top() != '[')
+                    return false;
+                st.pop();
+            }
+
+            else {
+                st.push(s[i]);
+            }
+        }
+
+        return st.empty();
+    }
+};
